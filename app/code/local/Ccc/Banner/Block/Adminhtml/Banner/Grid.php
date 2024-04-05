078800a0dc2865b9e9198a539a70873a0c03b929
BRP7 <bhumiparmar0007@gmail.com>
2024-04-05 11:15:29 +0530

    from show

--- app/code/local/Ccc/Banner/Block/Adminhtml/Banner/Grid.php
+++ app/code/local/Ccc/Banner/Block/Adminhtml/Banner/Grid.php
@@ -3,10 +3,35 @@ class Ccc_Banner_Block_Adminhtml_Banner_Grid extends Mage_Adminhtml_Block_Widget
 {
     protected function _prepareCollection()
     {
+        
+        $collection = Mage::getModel('ccc_banner/banner')->getCollection();
+        // echo get_class($collection);die;
+        //->getCollection()
+        // echo "<pre>";
+        // var_dump($collection);
+        
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
     }
 
     protected function _prepareColumns()
     {
         // Add columns for the grid
+        $this->addColumn('banner_id', array(
+            'header'    => Mage::helper('banner')->__('Banner ID'),
+            'align'     =>'right',
+            'width'     => '50px',
+            'index'     => 'banner_id',
+        ));
+    
+        $this->addColumn('banner_image', array(
+            'header'    => Mage::helper('banner')->__('Banner Image'),
+            'align'     =>'left',
+            'index'     => 'banner_img',
+        ));
+    
+        // Add more columns as needed
+    
+        return parent::_prepareColumns();
     }
 }
--- app/code/local/Ccc/Banner/controllers/Adminhtml/BannerController.php
+++ app/code/local/Ccc/Banner/controllers/Adminhtml/BannerController.php
@@ -3,6 +3,59 @@ class Ccc_Banner_Adminhtml_BannerController extends Mage_Adminhtml_Controller_Ac
 {
     public function indexAction()
     {
-        echo 12;
+        // echo 12;
+        $this->loadLayout();
+        $this->_title($this->__("Manage Banners"));
+        $this->renderLayout();
+    }
+
+    public function newAction(){
+        $this->_forward('edit');
+    }
+
+    protected function _initAction()
+    {
+        // load layout, set active menu and breadcrumbs
+        $this->loadLayout()
+            // ->_setActiveMenu('banner/banner')
+            // ->_addBreadcrumb(Mage::helper('banner')->__('BANNER'), Mage::helper('banner')->__('BANNER'))
+            // ->_addBreadcrumb(Mage::helper('banner')->__('Manage Pages'), Mage::helper('banner')->__('Manage Pages'))
+        ;
+        return $this;
+    }
+
+    public function editAction()
+    {
+        $this->_title($this->__('BANNER'))->_title($this->__('Static Blocks'));
+
+        // 1. Get ID and create model
+        $id = $this->getRequest()->getParam('banner_id');
+        $model = Mage::getModel('ccc_banner/banner');
+
+        // 2. Initial checking
+        if ($id) {
+            $model->load($id);
+            if (! $model->getId()) {
+                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('banner')->__('This block no longer exists.'));
+                $this->_redirect('*/*/');
+                return;
+            }
+        }
+
+        $this->_title($model->getId() ? $model->getTitle() : $this->__('New Block'));
+
+        // 3. Set entered data if was error when we do save
+        $data = Mage::getSingleton('adminhtml/session')->getFormData(true);
+        if (! empty($data)) {
+            $model->setData($data);
+        }
+
+        // 4. Register model to use later in blocks
+        Mage::register('banner_block', $model);
+
+        // 5. Build edit form
+        $this->_initAction()
+            ->_addBreadcrumb($id ? Mage::helper('banner')->__('Edit Block') : Mage::helper('banner')->__('New Block'), $id ? Mage::helper('banner')->__('Edit Block') : Mage::helper('banner')->__('New Block'))
+            ->renderLayout();
     }
 }
