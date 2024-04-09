dd5bc95ec932706d9987ded472839208fae9b611
BRP7 <bhumiparmar0007@gmail.com>
2024-04-09 18:16:23 +0530

    limit

--- app/code/local/Ccc/Banner/Block/Adminhtml/Banner/Grid.php
+++ app/code/local/Ccc/Banner/Block/Adminhtml/Banner/Grid.php
@@ -3,8 +3,18 @@ class Ccc_Banner_Block_Adminhtml_Banner_Grid extends Mage_Adminhtml_Block_Widget
 {
     protected function _prepareCollection()
     {
-        
+
         $collection = Mage::getModel('ccc_banner/banner')->getCollection();
+        $r=$collection->getSelect()->limit(4);
+        echo '<pre>';
+        // print_r($r);
+                    // $collection->setPageSize(2);
+            // $collection->setCurPage(1);
+
+
+        // if(Mage::getSingleton('admin/session')->isAllowed('ccc_banner/row/showall')){
+        // }
+
         // echo get_class($collection);die;
         //->getCollection()
         // echo "<pre>";
@@ -12,52 +22,83 @@ class Ccc_Banner_Block_Adminhtml_Banner_Grid extends Mage_Adminhtml_Block_Widget
         $this->setCollection($collection);
         return parent::_prepareCollection();
     }
-
+    public function checkColumn($column)
+    {
+        return Mage::getSingleton('admin/session')->isAllowed($column);
+    }
     protected function _prepareColumns()
     {
         // Add columns for the grid
-        $this->addColumn('banner_id', array(
-            'header'    => Mage::helper('banner')->__('Banner Id'),
-            'align'     =>'right',
-            'width'     => '50px',
-            'index'     => 'banner_id',
-        ));
-        $this->addColumn('banner_name', array(
-            'header'    => Mage::helper('banner')->__('Banner Name'),
-            'align'     =>'left',
-            'index'     => 'banner_name',
-        ));
-        $this->addColumn('banner_image', array(
-            'header'    => Mage::helper('banner')->__('Banner Image'),
-            'align'     => 'center',
-            'index'     => 'banner_image',
-            'renderer'  => 'Ccc_Banner_Block_Adminhtml_Banner_Grid_Renderer_Image', // Use custom renderer for image column
-        ));
+        if ($this->checkColumn('ccc_banner/columns/id')) {
+            $this->addColumn(
+                'banner_id',
+                array(
+                    'header' => Mage::helper('banner')->__('Banner Id'),
+                    'align' => 'right',
+                    'width' => '50px',
+                    'index' => 'banner_id',
+                )
+            );
+        }
+        if ($this->checkColumn('ccc_banner/columns/name')) {
+
+            $this->addColumn(
+                'banner_name',
+                array(
+                    'header' => Mage::helper('banner')->__('Banner Name'),
+                    'align' => 'left',
+                    'index' => 'banner_name',
+                )
+            );
+        }
+        if ($this->checkColumn('ccc_banner/columns/image')) {
+
+            $this->addColumn(
+                'banner_image',
+                array(
+                    'header' => Mage::helper('banner')->__('Banner Image'),
+                    'align' => 'center',
+                    'index' => 'banner_image',
+                    'renderer' => 'Ccc_Banner_Block_Adminhtml_Banner_Grid_Renderer_Image', // Use custom renderer for image column
+                )
+            );
+        }
         // $this->addColumn('banner_image', array(
         //     'header'    => Mage::helper('banner')->__('Banner Image'),
         //     'align'     =>'left',
         //     'index'     => 'banner_image',
         // ));
-        $this->addColumn('status', array(
-            'header'    => Mage::helper('banner')->__('Status'),
-            'align'     =>'left',
-            'index'     => 'status',
-        ));
-        $this->addColumn('show_on', array(
-            'header'    => Mage::helper('banner')->__('Show On'),
-            'align'     =>'left',
-            'index'     => 'show_on',
-        ));
-    
-        
-    
+        if ($this->checkColumn('ccc_banner/columns/status')) {
+
+            $this->addColumn(
+                'status',
+                array(
+                    'header' => Mage::helper('banner')->__('Status'),
+                    'align' => 'left',
+                    'index' => 'status',
+                )
+            );
+        }
+        if ($this->checkColumn('ccc_banner/columns/showon')) {
+
+            $this->addColumn(
+                'show_on',
+                array(
+                    'header' => Mage::helper('banner')->__('Show On'),
+                    'align' => 'left',
+                    'index' => 'show_on',
+                )
+            );
+        }
+
+
         // Add more columns as needed
-    
+
         return parent::_prepareColumns();
     }
     public function getRowUrl($row)
     {
-        if(Mage::getSingleton('admin/session')->isAllowed('ccc_banner/edit')){
+        if (Mage::getSingleton('admin/session')->isAllowed('ccc_banner/edit')) {
             return $this->getUrl('*/*/edit', array('banner_id' => $row->getId()));
         }
         return false;
@@ -68,34 +109,40 @@ class Ccc_Banner_Block_Adminhtml_Banner_Grid extends Mage_Adminhtml_Block_Widget
     {
         $this->setMassactionIdField('banner_id');
         $this->getMassactionBlock()->setFormFieldName('banner_id'); // Change to 'banner_id'
-    
-        $this->getMassactionBlock()->addItem('delete', array(
-            'label' => Mage::helper('banner')->__('Delete'),
-            'url'  => $this->getUrl('*/*/massDelete'),
-            'confirm' => Mage::helper('banner')->__('Are you sure you want to delete selected banners?')
-        ));
-    
+
+        $this->getMassactionBlock()->addItem(
+            'delete',
+            array(
+                'label' => Mage::helper('banner')->__('Delete'),
+                'url' => $this->getUrl('*/*/massDelete'),
+                'confirm' => Mage::helper('banner')->__('Are you sure you want to delete selected banners?')
+            )
+        );
+
         $statuses = Mage::getSingleton('ccc_banner/status')->getOptionArray();
-    
+
         array_unshift($statuses, array('label' => '', 'value' => ''));
-        $this->getMassactionBlock()->addItem('status', array(
-            'label' => Mage::helper('banner')->__('Change status'),
-            'url'  => $this->getUrl('*/*/massStatus', array('_current' => true)),
-            'additional' => array(
-                'visibility' => array(
-                    'name' => 'status',
-                    'type' => 'select',
-                    'class' => 'required-entry',
-                    'label' => Mage::helper('banner')->__('Status'),
-                    'values' => $statuses
+        $this->getMassactionBlock()->addItem(
+            'status',
+            array(
+                'label' => Mage::helper('banner')->__('Change status'),
+                'url' => $this->getUrl('*/*/massStatus', array('_current' => true)),
+                'additional' => array(
+                    'visibility' => array(
+                        'name' => 'status',
+                        'type' => 'select',
+                        'class' => 'required-entry',
+                        'label' => Mage::helper('banner')->__('Status'),
+                        'values' => $statuses
+                    )
                 )
             )
-        ));
-    
+        );
+
         Mage::dispatchEvent('banner_adminhtml_banner_grid_prepare_massaction', array('block' => $this));
         return $this;
     }
-    
+
 
 
 
