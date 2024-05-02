812b21cf6acdc6b0501523e6b17e87bbb952c3a1
BRP7 <bhumiparmar0007@gmail.com>
2024-05-02 11:24:07 +0530

    Update Based On Sku

--- app/code/local/Ccc/VendorInventory/Helper/VendorInventory.php
+++ app/code/local/Ccc/VendorInventory/Helper/VendorInventory.php
@@ -1,6 +1,7 @@
 <?php
 class Ccc_VendorInventory_Helper_Banner extends Mage_Core_Helper_Abstract
 {
+  
 
 }
 ?>
--- app/code/local/Ccc/VendorInventory/Model/Observer.php
+++ app/code/local/Ccc/VendorInventory/Model/Observer.php
@@ -2,52 +2,60 @@
 
 class Ccc_VendorInventory_Model_Observer
 {
-
-    public function readcsv()
+    public function readCsv()
     {
-        $attribute = Mage::getModel('eav/entity_attribute')->loadByCode('catalog_product', 'brand');
+    
+        $brands = Mage::helper('vendorinventory')->getBrandNames();
 
-        if ($attribute && $attribute->getId()) {
-            $options = $attribute->getSource()->getAllOptions(false);
-            foreach ($options as $option) {
-                if ($option['label'] == "CK") {
-                    $brandId = $option['value'];
-                    break;
-                }
+        foreach ($brands as $brandId => $_brandName) {
+           
+            if ($_brandName['label'] != 'CK') {
+                continue;
             }
-        }
-        $config = Mage::getModel('vendorinventory/vendorinventory')->load($brandId, 'brand_id');
-        $brandData = Mage::getModel('vendorinventory/configdata')->load($config->getId(), 'config_id');
-        $configData = json_decode($brandData->getBrandData());
-        echo "<pre>";
 
-        $path = Mage::getBaseDir('var') . DS . 'inventory' . DS . $brandId . DS . 'inventory.csv';
-        $row = 0;
-        $header = [];
-        $array = [];
-        if (($open = fopen($path, "r")) !== false) {
-            while (($data = fgetcsv($open, 1000, ",")) !== false) {
-                if (!$row) {
-                    $header = $data;
-                    $row++;
-                    continue;
-                }
-                $array = array_combine($header, $data);
+            $brandId = $_brandName['value'];
+
+
+            $collection = Mage::getModel('vendorinventory/configdata')
+                ->getCollection()
+                ->addFieldtoFilter('vendorinventory.brand_id', $brandId)
+                ->join('vendorinventory', 'main_table.id=vendorinventory.config_id');
+            $brandConfig = json_decode($collection->getFirstItem()->getBrandData());
+
+            if (is_null($brandConfig)){
+                // echo "Brand configuration not found for brand ID: $brandId\n";
+                continue;
+            }
+            // echo "Brand configuration found: " . print_r($brandConfig, true) . "\n";
+
+
+            $file = fopen(Mage::getBaseDir('var') . DS . 'inventory' . DS . $brandId . DS . 'inventory.csv', 'r');
+    
+            $headers = fgetcsv($file);
+            while ($row = fgetcsv($file)) {
+                $model = Mage::getModel("vendorinventory/items");
+                $data = array_combine($headers, $row);
                 $temp = [];
-                foreach ($configData as $_column => $_config) {
+                $temp['brand_id'] = $brandId;
+                foreach ($brandConfig as $_column => $_config) {
                     $dataColumn = '';
+                    $temp[$_column] = null;
                     $rule = [];
                     foreach ($_config as $_c) {
                         if (!is_string($_c)) {
-                            foreach ($_c as $_k => $_v) {
+                            foreach ($_c as $_k => $_v) { 
                                 $dataColumn = $_k;
-                                if($_column == 'sku'){
+                                if ($_column == 'sku') {
+                                    $itemCollection = $model->getCollection()->addFieldtoFilter('sku', $data[$_k]);
+                                    if($itemCollection->getFirstItem()->getId()){
+                                        $model->load($itemCollection->getFirstItem()->getId());
+                                    }
                                     $rule[] = true;
                                     break;
                                 }
                                 if ($_v->dataValue != '') {
                                     $rule[] = $this->checkRule(
-                                        $array[$_k],
+                                        $data[$_k],
                                         $_v->dataValue,
                                         $_v->dataType,
                                         $_v->operator
@@ -67,7 +75,6 @@ class Ccc_VendorInventory_Model_Observer
                             }
                         }
                     }
-
                     $result = false;
                     $logicalOperator = '';
                     foreach ($rule as $item) {
@@ -81,41 +88,46 @@ class Ccc_VendorInventory_Model_Observer
                             }
                         }
                     }
-                    if ($result)
-                        $temp[$_column] = $array[$dataColumn];
-
-                    $temp['sku'] = $array['sku'];
-
+                    if ($result){
+                        // print_r($_column  ."  ---  " . $data[$dataColumn]);
+                        // echo "    ";
+                        $temp[$_column] = $data[$dataColumn];
+                    }else{
+                        echo 121;
+                        $temp[$_column] = "Null";
+                        print($temp[$_column]);
+                    }
                 }
-                // print_r($temp);
-                $data = Mage::getModel("vendorinventory/items")->getCollection()->addFieldToFilter('brand_id', $brandId)->getFirstItem();
-                if ($data) {
-                    Mage::getModel("vendorinventory/items")->setData($temp)->addData(["brand_id" => $brandId, "brand_items_csv" => $data->getId()])->save();
-                } else {
-                    Mage::getModel("vendorinventory/items")->setData($temp)->addData(["brand_id" => $brandId])->save();
-                }
-
+                // echo "Temporary data array: " . print_r($temp, true) . "\n";
+                $model->addData($temp)->save();
+                // var_dump($temp);
             }
-        }
+        } 
     }
 
     public function checkRule($dataValue, $condValue, $condDataType, $condOperator)
     {
+        // echo 123;
         switch (strtolower($condDataType)) {
             case "count":
             case "number":
+                // echo 123;
+                // print_r($this->compareValues((int) $dataValue, (int) $condValue, $condOperator));
                 return $this->compareValues((int) $dataValue, (int) $condValue, $condOperator);
             case "text":
+                // print_r($this->compareValues(strtolower($dataValue), strtolower($condValue), $condOperator));
                 return $this->compareValues(strtolower($dataValue), strtolower($condValue), $condOperator);
             case "date":
                 $date1 = DateTime::createFromFormat('d-m-Y', $dataValue);
                 $date2 = DateTime::createFromFormat('d-m-Y', $condValue);
+                // print_r($this->compareValues($date1, $date2, $condOperator));
                 return $this->compareValues($date1, $date2, $condOperator);
         }
     }
 
     public function compareValues($value1, $value2, $operator)
     {
+        // echo 1213;
         switch ($operator) {
             case "=":
                 return $value1 == $value2;
@@ -132,122 +144,3 @@ class Ccc_VendorInventory_Model_Observer
         }
     }
 }
-
-
-// class Ccc_VendorInventory_Model_Observer
-// {
-//     public function readCsv()
-//     {
-//         echo "<pre>";
-//         $brands = Mage::helper('vendorinventory')->getBrandNames();
-
-//         foreach ($brands as $brandId => $_brandName) {
-//             $collection = Mage::getModel('vendorinventory/configdata')
-//                 ->getCollection()
-//                 ->addFieldtoFilter('vendorinventory.brand_id', $brandId)
-//                 ->join('vendorinventory', 'main_table.configuration_id=vendorinventory.configuration_id');
-//             $brandConfig = json_decode($collection->getFirstItem()->getBrandData());
-
-//             if (is_null($brandConfig))
-//                 continue;
-
-//             $file = fopen(Mage::getBaseDir('var') . DS . 'inventory' . DS . $brandId . DS . 'inventory.csv', 'r');
-//             $headers = fgetcsv($file);
-//             while ($row = fgetcsv($file)) {
-//                 $model = Mage::getModel("vendorinventory/items");
-//                 $data = array_combine($headers, $row);
-//                 $temp = [];
-//                 $temp['brand_id'] = $brandId;
-//                 foreach ($brandConfig as $_column => $_config) {
-//                     $dataColumn = '';
-//                     $temp[$_column] = null;
-//                     $rule = [];
-//                     foreach ($_config as $_c) {
-//                         if (!is_string($_c)) {
-//                             foreach ($_c as $_k => $_v) {
-//                                 $dataColumn = $_k;
-//                                 if ($_column == 'sku') {
-//                                     $itemCollection = $model->getCollection()->addFieldtoFilter('sku', $data[$_k]);
-//                                     if($itemCollection->getFirstItem()->getId()){
-//                                         $model->load($itemCollection->getFirstItem()->getId());
-//                                     }
-//                                     $rule[] = true;
-//                                     break;
-//                                 }
-//                                 if ($_v->condition_value != '') {
-//                                     $rule[] = $this->checkRule(
-//                                         $data[$_k],
-//                                         $_v->condition_value,
-//                                         $_v->data_type,
-//                                         $_v->condition_operator
-//                                     );
-//                                 } else {
-//                                     $rule[] = false;
-//                                 }
-//                             }
-//                         } else {
-//                             switch ($_c) {
-//                                 case "AND":
-//                                     $rule[] = "AND";
-//                                     break;
-//                                 case "OR":
-//                                     $rule[] = "OR";
-//                                     break;
-//                             }
-//                         }
-//                     }
-//                     $result = false;
-//                     $logicalOperator = '';
-//                     foreach ($rule as $item) {
-//                         if ($item === "AND" || $item === "OR") {
-//                             $logicalOperator = $item;
-//                         } else {
-//                             if ($logicalOperator === "AND") {
-//                                 $result = $result && $item;
-//                             } else {
-//                                 $result = $result || $item;
-//                             }
-//                         }
-//                     }
-//                     if ($result)
-//                         $temp[$_column] = $data[$dataColumn];
-//                 }
-//                 $model->addData($temp)->save();
-//                 print_r($temp);
-//             }
-//         }
-//     }
-
-//     public function checkRule($dataValue, $condValue, $condDataType, $condOperator)
-//     {
-//         switch (strtolower($condDataType)) {
-//             case "count":
-//             case "number":
-//                 return $this->compareValues((int) $dataValue, (int) $condValue, $condOperator);
-//             case "text":
-//                 return $this->compareValues(strtolower($dataValue), strtolower($condValue), $condOperator);
-//             case "date":
-//                 $date1 = DateTime::createFromFormat('m/d/Y', $dataValue);
-//                 $date2 = DateTime::createFromFormat('Y-m-d', $condValue);
-//                 return $this->compareValues($date1, $date2, $condOperator);
-//         }
-//     }
-
-//     public function compareValues($value1, $value2, $operator)
-//     {
-//         switch ($operator) {
-//             case "=":
-//                 return $value1 == $value2;
-//             case "!=":
-//                 return $value1 != $value2;
-//             case ">":
-//                 return $value1 > $value2;
-//             case ">=":
-//                 return $value1 >= $value2;
-//             case "<":
-//                 return $value1 < $value2;
-//             case "<=":
-//                 return $value1 <= $value2;
-//         }
-//     }
-// }
