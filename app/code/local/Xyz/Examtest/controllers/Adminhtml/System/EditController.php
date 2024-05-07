fba2b9076b10e818928a41819e9d3641be369fc5
BRP7 <bhumiparmar0007@gmail.com>
2024-05-08 00:16:54 +0530

    working on system config

--- app/code/local/Xyz/Examtest/controllers/Adminhtml/System/EditController.php
+++ app/code/local/Xyz/Examtest/controllers/Adminhtml/System/EditController.php
@@ -1,8 +1,15 @@
 <?php
 class Xyz_Examtest_Adminhtml_System_EditController extends Mage_Adminhtml_Controller_Action
 {
+
+    public function init()
+    {
+        die("Admin router initialized");
+        // Rest of your code here
+    }
     public function sectionAction()
     {
+        die("Reached here");
         // Your controller logic goes here
     }
 }
