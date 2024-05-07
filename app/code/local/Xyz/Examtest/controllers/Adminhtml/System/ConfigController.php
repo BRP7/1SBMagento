ca0d94dcd50664f4be468ee6e82e9139ec09b6a3
BRP7 <bhumiparmar0007@gmail.com>
2024-05-07 14:21:16 +0530

    updates

---
+++ app/code/local/Xyz/Examtest/controllers/Adminhtml/System/ConfigController.php
@@ -0,0 +1,8 @@
+<?php
+class Xyz_Examtest_Adminhtml_System_ConfigController extends Mage_Adminhtml_Controller_Action
+{
+    public function editAction()
+    {
+        // Your action logic here
+    }
+}
