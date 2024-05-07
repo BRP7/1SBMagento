47a49ec3f465f05561cbedb95a0004c72473e97a
BRP7 <bhumiparmar0007@gmail.com>
2024-05-07 14:05:33 +0530

    updates

---
+++ app/code/local/Xyz/Examtest/Helper/Data.php
@@ -0,0 +1,11 @@
+<?php 
+
+// class Xyz_Examtest_Helper_Data extends Mage_Core_Helper_Abstract{
+
+// }
+
+
+class Xyz_Examtest_Helper_Data extends Mage_Core_Helper_Abstract
+{
+    // Your helper methods here
+}
