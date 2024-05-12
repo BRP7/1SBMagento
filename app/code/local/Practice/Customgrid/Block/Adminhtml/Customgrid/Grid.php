fb0db2ebeb38769a8ba006b87d4f2e9b7bbd1335
BRP7 <bhumiparmar0007@gmail.com>
2024-05-12 10:50:58 +0530

    next level of grid adding and displaying data

--- app/code/local/Practice/Customgrid/Block/Adminhtml/Customgrid/Grid.php
+++ app/code/local/Practice/Customgrid/Block/Adminhtml/Customgrid/Grid.php
@@ -1,7 +1,111 @@
 <?php
 
-class Practice_Customgrid_Block_Adminhtml_Customgrid_Grid extends Mage_Adminhtml_Block_Widget_Grid {
-    public function __construct($attributes=array()) {
-        parent::__construct($attributes);
+class Practice_Customgrid_Block_Adminhtml_Customgrid_Grid extends Mage_Adminhtml_Block_Widget_Grid
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->setId('customgridGrid');
+        $this->setDefaultSort('customgrid_id');
+        $this->setDefaultDir('ASC');
+        $this->setSaveParametersInSession(true);
+    }
+
+    protected function _prepareCollection()
+    {
+        $collection = Mage::getModel('practice_customgrid/customgrid')->getCollection();
+        $this->setCollection($collection);
+        return parent::_prepareCollection();
+    }
+
+    protected function _prepareColumns()
+    {
+        $this->addColumn('customgrid_id', array(
+            'header' => Mage::helper('practice_customgrid')->__('ID'),
+            'align' => 'right',
+            'width' => '50px',
+            'index' => 'customgrid_id',
+        ));
+
+        $this->addColumn('grid_name', array(
+            'header' => Mage::helper('practice_customgrid')->__('Grid Name'),
+            'align' => 'left',
+            'index' => 'grid_name',
+        ));
+
+        $this->addColumn('show_on', array(
+            'header' => Mage::helper('practice_customgrid')->__('Show On'),
+            'align' => 'left',
+            'index' => 'show_on',
+        ));
+
+        $this->addColumn('status', array(
+            'header' => Mage::helper('practice_customgrid')->__('Status'),
+            'align' => 'left',
+            'width' => '80px',
+            'index' => 'status',
+            'type' => 'options',
+            'options' => array(
+                1 => Mage::helper('practice_customgrid')->__('Enabled'),
+                2 => Mage::helper('practice_customgrid')->__('Disabled'),
+            ),
+        ));
+
+        $this->addColumn('action', array(
+            'header' => Mage::helper('practice_customgrid')->__('Action'),
+            'width' => '100px',
+            'type' => 'action',
+            'getter' => 'getId',
+            'actions' => array(
+                array(
+                    'caption' => Mage::helper('practice_customgrid')->__('Edit'),
+                    'url' => array('base' => '*/*/edit'),
+                    'field' => 'id'
+                ),
+            ),
+            'filter' => false,
+            'sortable' => false,
+            'index' => 'stores',
+        ));
+
+        return parent::_prepareColumns();
+    }
+
+    protected function _prepareMassaction()
+    {
+        $this->setMassactionIdField('customgrid_id');
+        $this->getMassactionBlock()->setFormFieldName('customgrid');
+
+        $this->getMassactionBlock()->addItem('delete', array(
+            'label' => Mage::helper('practice_customgrid')->__('Delete'),
+            'url' => $this->getUrl('*/*/massDelete'),
+            'confirm' => Mage::helper('practice_customgrid')->__('Are you sure?')
+        ));
+
+        $statuses = array(
+            1 => Mage::helper('practice_customgrid')->__('Enabled'),
+            2 => Mage::helper('practice_customgrid')->__('Disabled')
+        );
+
+        array_unshift($statuses, array('label' => '', 'value' => ''));
+        $this->getMassactionBlock()->addItem('status', array(
+            'label' => Mage::helper('practice_customgrid')->__('Change status'),
+            'url' => $this->getUrl('*/*/massStatus', array('_current' => true)),
+            'additional' => array(
+                'visibility' => array(
+                    'name' => 'status',
+                    'type' => 'select',
+                    'class' => 'required-entry',
+                    'label' => Mage::helper('practice_customgrid')->__('Status'),
+                    'values' => $statuses
+                )
+            )
+        ));
+        return $this;
+    }
+
+    public function getRowUrl($row)
+    {
+        return $this->getUrl('*/*/edit', array('id' => $row->getId()));
     }
 }
--- app/code/local/Practice/Customgrid/controllers/Adminhtml/CustomgridController.php
+++ app/code/local/Practice/Customgrid/controllers/Adminhtml/CustomgridController.php
@@ -5,7 +5,75 @@ class Practice_Customgrid_Adminhtml_CustomgridController extends Mage_Adminhtml_
 {
     public function indexAction()
     {
-        $this->loadLayout();
+        $this->_initAction();
         $this->renderLayout();
     }
+    public function viewAction()
+    {
+        echo 123;
+    }
+    protected function _initAction()
+{
+    // Load layout, set active menu, and breadcrumbs
+    $this->loadLayout()
+        ->_setActiveMenu('practice_customgrid')
+        ->_addBreadcrumb(Mage::helper('adminhtml')->__('Customgrid'), Mage::helper('adminhtml')->__('Customgrid'))
+        ->_addBreadcrumb(Mage::helper('adminhtml')->__('Manage Customgrid'), Mage::helper('adminhtml')->__('Manage Customgrid'));
+    return $this;
+}
+
+
+    public function newAction(){
+        $this->_forward('edit');
+    }
+
+    public function editAction()
+    {
+        // Set the page title
+        $this->_title($this->__('Manage Customgrid'));
+    
+        // Get the customgrid ID from the request parameters
+        $id = $this->getRequest()->getParam('customgrid_id');
+    
+        // Load the customgrid model based on the ID
+        $model = Mage::getModel('practice_customgrid/customgrid');
+    
+        // Initial checking
+        if ($id) {
+            $model->load($id);
+            if (!$model->getId()) {
+                Mage::getSingleton('adminhtml/session')->addError(Mage::helper('practice_customgrid')->__('This customgrid does not exist.'));
+                $this->_redirect('*/*/');
+                return;
+            }
+        }
+    
+        // Set the page title based on whether it's a new customgrid or an edit action
+        $this->_title($model->getId() ? $this->__('Edit Customgrid') : $this->__('New Customgrid'));
+    
+        // Set entered data if there was an error during saving
+        $data = Mage::getSingleton('adminhtml/session')->getFormData(true);
+        if (!empty($data)) {
+            $model->setData($data);
+        }
+    
+        // Register the customgrid model to use it later in blocks
+        Mage::register('customgrid_data', $model);
+    
+        // Initialize the action
+        $this->_initAction();
+    
+        // Add breadcrumbs
+        $this->_addBreadcrumb($id ? $this->__('Edit Customgrid') : $this->__('New Customgrid'), $id ? $this->__('Edit Customgrid') : $this->__('New Customgrid'));
+    
+        // Get the edit form block
+        $editBlock = $this->getLayout()->createBlock('practice_customgrid/adminhtml_customgrid_edit');
+    
+        // Set the form action URL
+        $editBlock->setData('action', $this->getUrl('*/*/save'));
+    
+        // Render layout
+        $this->renderLayout();
+    }
+    
 }
