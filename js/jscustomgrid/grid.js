2f2427189dade5bf06793c211be47f3c925074bb
BRP7 <bhumiparmar0007@gmail.com>
2024-05-19 19:02:05 +0530

    Perfect

--- js/jscustomgrid/grid.js
+++ js/jscustomgrid/grid.js
@@ -93,41 +93,94 @@ function updateCustomAttribute(productId, value, url, fromId) {
 
 
 
+// jQuery(document).ready(function($) {
+//     $('.product-name').each(function(index) {
+//         $(this).click(function() {
+//             // var productId = $(this).data('id');
+//                     var index = $(this).closest('tr').index(); // Get the index of the clicked row
+//                     var productId = productIds[index]; // Access the corresponding product ID from the array
+//                     var updateUrl = window.updateUrl;
+//                     var formKey = window.formKey;
+                    
+//                     console.log(productId);
+//                     console.log(updateUrl);
+//                     console.log(formKey);
+                    
+          
+//             // Add additional console logs or alerts to debug further
+            
+//             $.ajax({
+//                 url: updateUrl,
+//                 method: 'post',
+//                 data: { id: productId },
+//                 success: function(response) {
+//                     var product = $.parseJSON(response);
+//                     var modalHtml = '<div id="productModal" title="Product Details">' +
+//                                     '<p>Name: ' + product.name + '</p>' +
+//                                     '<p>Custom Attribute: ' + product.custom_attribute + '</p>' +
+//                                     '</div>';
+//                     $('body').append(modalHtml);
+//                     $('#productModal').dialog({
+//                         modal: true,
+//                         buttons: {
+//                             Ok: function() {
+//                                 $(this).dialog('close');
+//                                 $('#productModal').remove();
+//                             }
+//                         }
+//                     });
+//                 },
+//                 error: function(xhr, status, error) {
+//                     console.log(xhr.responseText);
+//                     console.log(status);
+//                     console.log(error);
+//                 }
+//             });
+//         });
+//     });
+// });
+
+
 jQuery(document).ready(function($) {
     $('.product-name').each(function(index) {
         $(this).click(function() {
-            // var productId = $(this).data('id');
-                    var index = $(this).closest('tr').index(); // Get the index of the clicked row
-                    var productId = productIds[index]; // Access the corresponding product ID from the array
-                    var updateUrl = window.updateUrl;
-                    var formKey = window.formKey;
-                    
-                    console.log(productId);
-                    console.log(updateUrl);
-                    console.log(formKey);
-                    
-          
-            // Add additional console logs or alerts to debug further
-            
+            var productId = productIds[index]; // Access the corresponding product ID from the array
+            var updateUrl = window.updateUrl;
+            var formKey = window.formKey;
+
+            console.log(productId);
+            console.log(updateUrl);
+            console.log(formKey);
+
             $.ajax({
                 url: updateUrl,
                 method: 'post',
-                data: { id: productId },
+                data: { id: productId ,from_id:formKey },
                 success: function(response) {
+                    console.log(response);
                     var product = $.parseJSON(response);
                     var modalHtml = '<div id="productModal" title="Product Details">' +
                                     '<p>Name: ' + product.name + '</p>' +
-                                    '<p>Custom Attribute: ' + product.custom_attribute + '</p>' +
+                                    '<p>Custom Attribute: ' + product.custom_select_attribute + '</p>' +
                                     '</div>';
                     $('body').append(modalHtml);
-                    $('#productModal').dialog({
-                        modal: true,
-                        buttons: {
-                            Ok: function() {
-                                $(this).dialog('close');
-                                $('#productModal').remove();
-                            }
-                        }
+
+                    // Open modal dialog using simple jQuery
+                    $('#productModal').css({
+                        'position': 'fixed',
+                        'top': '50%',
+                        'left': '50%',
+                        'transform': 'translate(-50%, -50%)',
+                        'background': '#fff',
+                        'border': '1px solid #000',
+                        'padding': '20px',
+                        'box-shadow': '0 0 10px rgba(0,0,0,0.3)',
+                        'z-index': '9999'
+                    }).show();
+
+                    // Close modal dialog on click
+                    $('#productModal').click(function() {
+                        $(this).hide().remove();
                     });
                 },
                 error: function(xhr, status, error) {
