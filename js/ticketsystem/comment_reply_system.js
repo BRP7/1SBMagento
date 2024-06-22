aea285689b1262641181f8525fb2efae9efd417e
BRP7 <bhumiparmar0007@gmail.com>
2024-06-22 23:41:02 +0530

    Js Reply Of Comment

--- js/ticketsystem/comment_reply_system.js
+++ js/ticketsystem/comment_reply_system.js
@@ -117,8 +117,6 @@ j(document).ready(function() {
         }
     });
 
-    
-
     // Lock button click event
     j('#dynamicTable').on('click', '.lock', function() {
         var allSaved = true;
